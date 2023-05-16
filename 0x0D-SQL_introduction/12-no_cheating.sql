fed69e3c21558305914ea5ddfc5d0ed8c74ab873
yaredybk <yb12ybk@gmail.com>
2023-05-16 15:52:29 -0700

    new files

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
---updates the score of Bob to 10 in the table second_table.
---You are not allowed to use Bob’s id value, only the name field
+-- updates the score of Bob to 10 in the table second_table.
+-- You are not allowed to use Bob’s id value, only the name field
 UPDATE second_table SET score = 10 WHERE name = 'Bob';
 
