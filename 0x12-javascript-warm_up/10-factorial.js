da3dfbf1c414a33af49c4258b04b49139908877a
yaredybk <yb12ybk@gmail.com>
2023-07-02 14:50:12 -0700

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+	return n === 2 ? 2 : n * factorial(n - 1);
 }
-
+if(process.argv[2] === 0 || isNaN(process.argv[2])){
+	console.log(1);
+}
+else{
+	console.log(factorial(process.argv[2]));
+}
 
