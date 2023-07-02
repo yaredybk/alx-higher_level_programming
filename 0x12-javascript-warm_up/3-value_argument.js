866051ee340427a3806a6e6b0abef8ffd3eb9b7e
yaredybk <yb12ybk@gmail.com>
2023-07-02 14:21:21 -0700

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(process.argv[0] ? process.argv[0] : 'No argument');
