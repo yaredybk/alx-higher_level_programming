9914c06b994c23f8eccc19fa8c906c0725e5fd04
yaredybk <yb12ybk@gmail.com>
2024-01-08 02:37:21 -0800

    update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    length = len(argv)
+
+    if(length == 1):
+        print("{:d} arguments.".format(length - 1))
+    else:
+        if(length == 2):
+            print("{:d} argument.".format(length - 1))
+        else:
+            print("{:d} arguments.".format(length - 1))
+        for i in range(1, length):
+            print("{:d}: {:s}".format(i, argv[i]))
