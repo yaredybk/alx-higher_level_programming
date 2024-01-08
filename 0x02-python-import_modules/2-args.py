92a0cbcfa423b61fd5b833791d4a393a747892de
yaredybk <yb12ybk@gmail.com>
2024-01-08 02:40:35 -0800

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
         print("{:d} arguments.".format(length - 1))
     else:
         if(length == 2):
-            print("{:d} argument.".format(length - 1))
+            print("{:d} argument:".format(length - 1))
         else:
-            print("{:d} arguments.".format(length - 1))
+            print("{:d} arguments:".format(length - 1))
         for i in range(1, length):
             print("{:d}: {:s}".format(i, argv[i]))
