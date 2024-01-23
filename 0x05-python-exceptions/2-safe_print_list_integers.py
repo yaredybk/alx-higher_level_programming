b3e70bfd1f5fd4dd5bb6a68fe32a65d8910ed2d1
yaredybk <yb12ybk@gmail.com>
2024-01-23 05:00:14 -0800

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-    print("",end="\n")
+    print("", end="\n")
     return (count)
