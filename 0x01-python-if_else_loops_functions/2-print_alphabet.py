9705abc6c60c65eeb29e6908ca4a1dd610ef540e
yaredybk <yb12ybk@gmail.com>
2024-01-23 20:45:03 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/bin/usr/python3
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/ust/bin/python3
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:0>2}".format(number), end=", ")
