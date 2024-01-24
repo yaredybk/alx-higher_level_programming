8b81c1c011d10abc815d3a4acf5c39585d2e0992
yaredybk <yb12ybk@gmail.com>
2024-01-24 01:39:20 -0800

    hot fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-def safe_function(fct,*args):
+
+def safe_function(fct, *args):
     try:
         res = fct(*args)
         return (res)
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
