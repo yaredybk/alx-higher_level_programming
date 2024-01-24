095ab4b815bfdd433e303f598add01cfba9eea1d
yaredybk <yb12ybk@gmail.com>
2024-01-24 14:27:16 -0800

    update

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    let newm = []
+    for ele in matrix:
+        tmp = []
+        for ele2 in ele:
+            tmp.append(ele2 *ele2)
+        newm.append(tmp)
+    return (newm)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,4 @@
+#0x04. Python - More Data Structures: Set, Dictionary
+Python
+ By: Guillaume
+ Weight: 1
