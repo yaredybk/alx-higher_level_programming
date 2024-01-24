2e838f7a7cfbc511956d9e7b4375b1c1d9de3c2f
yaredybk <yb12ybk@gmail.com>
2024-01-23 23:56:11 -0800

    hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@ def no_c(my_string):
     tmp = ""
     for i in range(len(my_string)):
         char = my_string[i]
-        if (char != 'c' or char != 'C'):
-            tmp += char
+        if (char != 'c' and char != 'C'):
+            tmp = tmp.join(char)
     return (tmp)
