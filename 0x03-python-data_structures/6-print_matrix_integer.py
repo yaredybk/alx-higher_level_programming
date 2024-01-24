08f05a3e48e9b4b4098d88167127f4964cef0919
yaredybk <yb12ybk@gmail.com>
2024-01-24 00:11:54 -0800

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for l1 in matrix:
+        len1 = len(l1)
+        for i in range(len1):
+            print("{:d}".format(ele))
+            if (i == len1 -1):
+                print("", end="\n")
+            else:
+                print(" ", end="")
