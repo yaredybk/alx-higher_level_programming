475a735c91d0ca7ec2ef6897af12495d0be6eb8f
yaredybk <yb12ybk@gmail.com>
2024-01-25 16:01:12 -0800

    hot fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    let newm = matrix.copy()
+    newm = matrix.copy()
     for ind in range(len(matrix) - 1):
-        for ind2 in range(len(matrix[ind] - 1):
+        for ind2 in range(len(matrix[ind]) - 1):
+            ele2 = matrix[ind][ind2]
             newm[ind][ind2] = (ele2 * ele2)
     return (newm)
