4efd5bddb1a6dd839fe1377886f4376a847c293f
yaredybk <yb12ybk@gmail.com>
2024-02-01 05:43:27 -0800

    update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+define a square class
+"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """
+        initialize square
+        args:
+            size (int): size of square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns the zise attribute"""
+        return self.__seze
+
+    def area(self):
+        """returns the current square are"""
+        return (self.__size**2)
