61b24d7ddfbd0e1caf6cea149d86c7a0ab42e87d
yaredybk <yb12ybk@gmail.com>
2024-01-31 23:22:13 -0800

    update

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,10 @@ class Rectangle:
     """Represent a rectangle."""
 
     # counter class variable
-    __number_of_instances = 0
+    number_of_instances = 0
+
+    # print symbole
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -17,7 +20,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.__number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -45,10 +48,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def number_of_instances():
-        """returns the number of instances"""
-        return (__number_of_instances)
-
     def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
@@ -64,7 +63,7 @@ class Rectangle:
         str = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                str += "#"
+                str += print_symbol
             str += "\n"
         return str[:-1]
 
@@ -75,4 +74,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.__number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
