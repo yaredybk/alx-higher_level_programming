a035489ae7c134c8686cc76eaf6aa1aa24697d7d
yaredybk <yb12ybk@gmail.com>
2024-02-01 05:55:24 -0800

    hot fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -32,8 +32,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
-
     def area(self):
         """returns the current square are"""
         return (self.__size**2)
