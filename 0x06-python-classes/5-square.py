991b6572a539dfdb7eb7ad4c4ceec39cd4da0120
yaredybk <yb12ybk@gmail.com>
2024-02-01 06:01:32 -0800

    update

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,8 +22,25 @@ class Square:
     @property
     def size(self):
         """returns the zise attribute"""
-        return self.__size
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """returns the current square are"""
         return (self.__size**2)
+
+    def my_print(self):
+        """prints a string representation"""
+        tmp_s = ""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                tmp_s += "#"
+            tmp_s += "\n"
+        return tmp_s[:-1]
