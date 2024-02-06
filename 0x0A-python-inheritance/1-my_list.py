4eedd876099e8a15d207a15bac061aac1e15d874
yaredybk <yb12ybk@gmail.com>
2024-02-05 18:34:39 -0800

    hot fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,5 @@
 def MyList(list):
     """ list class with a print_sorted method"""
 
-    def __init__(self):
-        super().__init__()
-
-    """ print the list in asce order"""
-
     def print_sorted(self):
         print(self.sort())
