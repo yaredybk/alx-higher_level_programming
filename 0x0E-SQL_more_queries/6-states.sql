2efc8c987353e92b19ec3d24c8205eaf81077a6b
yaredybk <yb12ybk@gmail.com>
2024-03-13 10:46:20 -0700

    hot fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,6 +4,7 @@
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `states` (
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
 	`name` VARCHAR(256) NOT NULL
