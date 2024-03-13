c911d354d81c76c6baf3aebdd5c37e5f584952bb
yaredybk <yb12ybk@gmail.com>
2024-03-13 12:55:22 -0700

    update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,8 +7,6 @@ SELECT tg.name AS name
 FROM tv_show_genres tsg
 JOIN tv_genres tg
 ON tg.id = tsg.genre_id
-WHERE tsg.show_id = (
-	SELECT id
-	FROM tv_shows
-	WHERE title = 'Dexter'
-)
+JOIN tv_shows ts
+ON ts.id = tsg.show_id
+WHERE ts.title = 'Dexter';
