de015375b6a899bcbe9ae142d4ef0f8b8c497c53
yaredybk <yb12ybk@gmail.com>
2024-03-13 09:25:46 -0700

    hot fix

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 SELECT `city`, AVG(`value`) as `avg_temp`
 FROM `temperatures`
-WHERE `month` = 'July' OR `month` = 'August'
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
