d412ea172a7820919628e6d63ba051c99eb7c88a
yaredybk <yb12ybk@gmail.com>
2024-03-13 08:24:47 -0700

    hot fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,7 +6,7 @@
 -- Field name in first_table
 USE `hbtn_0c_0`;
 ALTER DATABASE `hbtn_0c_0`
-CHARACTEER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
