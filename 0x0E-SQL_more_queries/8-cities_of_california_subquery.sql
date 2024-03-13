a8fb52536dd94ae27c89a52c38d21c76a08b81a3
yaredybk <yb12ybk@gmail.com>
2024-03-13 11:25:00 -0700

    hot fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,7 @@
 -- 
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
-SELECT * FROM `cities`
+SELECT `id`, `name` FROM `cities`
 WHERE `state_id` = (
 	SELECT `id` FROM `states`
 	WHERE `name` = 'California'
