333567019b5e8f4c9c9fbdf2f06a01c6db55b291
yared bekuru <yb12ybk@gmail.com>
2024-04-26 03:07:06 -0700

    hot fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-let v = Integer.parseint(argv[2]);
+let v = Number(argv[2]);
 if (v) console.log(`My number: ${v}`);
 else console.log('Not a number');
