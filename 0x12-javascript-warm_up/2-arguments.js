dba9aea653729463839ddcf74645ca15f6c66d7b
yared bekuru <yb12ybk@gmail.com>
2024-04-26 01:18:39 -0700

    hot fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const {argv} = require('node:process');
 
-if (argv.lenght === 2) return console.log('No argument');
-if (argv.lenght === 3) return console.log('Argument found');
-return console.log('Arguments found');
+if (argv.length=== 2) console.log('No argument');
+else if (argv.length === 3) console.log('Argument found');
+else console.log('Arguments found');
