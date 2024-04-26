e61495fee2be83adf8d041330241515b3d743e66
yared bekuru <yb12ybk@gmail.com>
2024-04-26 14:53:25 -0700

    hot fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,5 @@
 const { argv } = require('node:process');
 
 let v = Number(argv[2]);
-if (v) {
-	while (v--) console.log('C is fun');
-} else {
-	console.log('Missing number of occurrences');
-}
+if (v) while (v--) console.log('C is fun');
+else console.log('Missing number of occurrences');
