6e7d03c9bf159e03bea45974eb2cdbbef017e2af
yared bekuru <yb12ybk@gmail.com>
2024-04-27 13:02:43 -0700

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
 
+function factorial (x) {
+	if
+const v = Number(argv[2]);
+const w = Number(argv[3]);
+console.log(v + w);
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,6 @@
 const { argv } = require('node:process');
 
 const v = Number(argv[2]);
-console.log(v);
 if (v) {
   let i = v, j = v, s = '';
   while (i--) s += 'X';
