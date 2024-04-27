3c5cb8f397a9c085de5cb1d5a6722630f8d255c8
yared bekuru <yb12ybk@gmail.com>
2024-04-26 21:44:40 -0700

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
 
+const v = Number(argv[2]);
+console.log(v);
+if (v) {
+  let i = v, j = v, s = '';
+  while (i--) s += 'X';
+  while (j--) console.log(s);
+} else console.log('Missing size');
