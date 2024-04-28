1ec2473463321e6a5e41d46313812eea64a2aca6
yared bekuru <yb12ybk@gmail.com>
2024-04-28 01:22:09 -0700

    hot fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = () => (myObject.value = myObject.value++);
+myObject.incr = () => this.value++;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
