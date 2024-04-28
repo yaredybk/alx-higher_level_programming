14e584d0086844f50e9f425a4c6f97d7861429b0
yared bekuru <yb12ybk@gmail.com>
2024-04-28 01:02:07 -0700

    hot fix

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction = () => null) {
   while (x--) theFunction();
-}
+};
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-
+exports.callMeMoby = function (number, theFunction = () => null) {
+  theFunction(Number(number)++);
+};
