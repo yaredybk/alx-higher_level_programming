bdb5b211f2fab595703371ac9e1d47492a25873b
yared b <yb12ybk@gmail.com>
2024-04-28 16:02:00 -0700

    new file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#1/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c = 'X') {
+    let s = '';
+    for (let i = 0; i < this.width; i++) s = s + c;
+    for (let i = 0; i < this.height; i++) {
+      console.log(s);
+    }
+
+  }
+};
