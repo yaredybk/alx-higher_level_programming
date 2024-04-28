c152926570efcfbb997faa7e9b0dd26e5c0766cc
yared bekuru <yb12ybk@gmail.com>
2024-04-28 01:31:25 -0700

    hot fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const v = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+const v = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while (v[i]) {
-  console.log(v[i]);
-  i++;
-}
+for (let i = 0; i < 3; i++) console.log(v[i]);
