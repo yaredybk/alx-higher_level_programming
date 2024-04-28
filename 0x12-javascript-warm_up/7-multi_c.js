60924606e3cca765d4cbbf7e0cc5d3329499cf30
yared bekuru <yb12ybk@gmail.com>
2024-04-28 01:33:19 -0700

    hot fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-let v = Math.floor(Number(argv[2]));
-if (isNaN(v)) console.log('Missing number of occurrences');
-else while (v--) console.log('C is fun');
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
