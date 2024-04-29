c75c5e908834ddcd966af25561661d9aa7c28de3
yared b <yb12ybk@gmail.com>
2024-04-28 17:09:57 -0700

    new file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const { dict } = require('./101-data.js');
 let sorted = {};
-Object.keys(dict).forEach(k => sorted[dict[k]]
-	? sorted[dict[k]].push(k)
-	: sorted[dict[k]] = [k]);
+Object.keys(dict).forEach((k) => {
+  if (sorted[dict[k]] == undefined) sorted[dict[k]] = [k];
+  else sorted[dict[k]].push(k);
+}
 console.log(sorted);
