b231d56cc370fbe8c19caa17a0264ee08ddc9a95
yaredybk <yb12ybk@gmail.com>
2024-04-30 00:33:46 -0700

    hot fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,9 @@ if __name__ == '__main__':
             user=argv[1], passwd=argv[2], db=argv[3]
     )
     c = db.cursor()
-    query = "SELECT `id`, `name` FROM `states` WHERE `states`.`name` = '{}' \
-              ORDER BY `states`.`id` DESC".format(argv[4])
+    query = "SELECT `id`, `name` FROM `states` \
+            WHERE `states`.`name` REGEXP '{}' \
+            ORDER BY `states`.`id` DESC".format(argv[4])
     c.execute(query)
     for s in c.fetchall():
         print(s)
