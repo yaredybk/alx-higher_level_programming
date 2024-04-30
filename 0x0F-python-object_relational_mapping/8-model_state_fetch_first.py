8ddc5910d201ac20f09a459934f8e2d2adb0cf2b
yaredybk <yb12ybk@gmail.com>
2024-04-29 21:36:58 -0700

    restartig

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+#./8-model_state_fetch_first.py
+#!/usr/bin/python3
 """
 ORM python3
 """
@@ -7,4 +9,9 @@ from sys import argv
 
 # do not execute this at import
 if __name__ == '__main__':
-	pass
+    db = MySQLdb.connect(
+            user=argv[1], passwd=argv[2], db=argv[3]
+    )
+    c = db.cursor()
+    c.execute("SELECT `id`, `name` FROM `states` ORDER BY `state`.`id`")
+    [print(f"({id}, '{state}'") for state in c.fetchall() if state[1][0] == "N"]
