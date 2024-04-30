9fbc2f2329553cb683b5ca9f63ca38c2933c3bdc
yaredybk <yb12ybk@gmail.com>
2024-04-30 01:59:05 -0700

    hot fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,14 +3,18 @@
 ORM python3
 """
 
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 # do not execute this at import
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-            user=argv[1], passwd=argv[2], db=argv[3]
-    )
-    c = db.cursor()
-    query = "SELECT `id`, `name` FROM `states` WHERE `states`.`name` = '{}' \
-              ORDER BY `states`.`id`".format(argv[4])
-    c.execute(query)
-    [print(state) for state in c.fetchall()]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for s in session.query(State).order_by(State.id):
+        print("{}: {}".format(s.id, s.name))
