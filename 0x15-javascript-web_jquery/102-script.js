58c1c73c9cf5a405839ff3d4c03930a3d9aaa1fc
yaredybk <yb12ybk@gmail.com>
2024-05-30 14:00:44 -0700

    running_to_deadline

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@
 // 
 // You should use this API service: https://www.fourtonfish.com/hellosalut/hello/
 // The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
-// The translation must be fetched when the user clicks on INPUT#btn_translate OR presses ENTER when the focus is on INPUT#language_code
+// The translation must be fetched when the user clicks on INPUT#btn_translate
 // The translation of “Hello” must be displayed in the HTML tag DIV#hello
 // You can’t use document.querySelector to select the HTML tag
 // You must use the JQuery API
